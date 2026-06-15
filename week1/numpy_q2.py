178b7ba107ecf21ac66c09709226218b90bae4df
Roya Sotoude <118993192+royasotude@users.noreply.github.com>
2026-06-15 16:29:23 +0330

    Delete week1/numpy_q2.py

--- week1/numpy_q2.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def p2p_distance(input):
-    input = np.array(input)
-    dist = input[:, np.newaxis, :] - input[np.newaxis, :, :]
-    output = np.sqrt(np.sum(dist**2, axis=2))
-    return output
